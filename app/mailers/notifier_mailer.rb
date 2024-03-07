8ea9149d228a75933cf12eb37207daeffac9ad91
Julia Grimes <julia.s.grimes@gmail.com>
2024-03-07 02:06:08 -0500

    Forward to mike

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,11 +1,11 @@
 class NotifierMailer < ApplicationMailer
-  default to: "julia.s.grimes@gmail.com",
-    from: "julia@juliasuzannegrimes.com"
+  default to: "mike.grimes@ecrebo.com",
+    from: "ecrebocpr@juliasuzannegrimes.com"
 
   def simple_message(first_name, last_name, email, message)
     mail(
       "reply-to": email_address_with_name(email, "#{first_name} #{last_name}"),
-      subject: "New contact form message",
+      subject: "New message from EcreboCPR site",
       body: message,
     )
   end
